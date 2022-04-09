c30fe7ac6ea91f815c140f2a869f991375c3a3b3
Neeraj Dalal <admin@nrjdalal.com>
2022-04-09 20:31:15 +0530

    Sat Apr  9 20:31:15 IST 2022

--- compile.js
+++ compile.js
@@ -1,10 +1,10 @@
 import { globby } from 'globby'
 import { exec } from 'child_process'
 
-const paths = await globby('./src/**/*.mjml')
+const files = await globby('./src/**/*.mjml')
 
-for (let index = 0; index < paths.length; index++) {
-  const src = paths[index]
+for (let index = 0; index < files.length; index++) {
+  const src = files[index]
   const dist = src.slice(0, src.lastIndexOf('/') + 1).replace('./src/', './public/')
 
   exec(`mkdir -p '${dist}' && npx mjml '${src}' -o '${dist}'`)
