a1fc5955fa8faff361dca7ae8cda85aa23afa530
Neeraj Dalal <admin@nrjdalal.com>
2022-04-09 17:34:02 +0530

    Sat Apr  9 17:34:02 IST 2022

--- index.js
+++ index.js
@@ -3,22 +3,14 @@ import { exec } from 'child_process'
 
 const dirs = []
 
-const compile = async () => {
-  const paths = await globby('./src/**/*.mjml')
+const paths = await globby('./src/**/*.mjml')
 
-  for (let index = 0; index < paths.length; index++) {
-    const src = paths[index].slice(0, paths[index].lastIndexOf('/') + 1)
+for (let index = 0; index < paths.length; index++) {
+  const src = paths[index].slice(0, paths[index].lastIndexOf('/') + 1)
 
-    if (!dirs.includes(src)) {
-      const dist = src.slice(0, src.lastIndexOf('/') + 1).replace('./src/', './dist/')
-      exec(`mkdir -p '${dist}' && npx mjml -w '${src}/*.mjml' -o '${dist}'`)
-      dirs.push(src)
-    }
+  if (!dirs.includes(src)) {
+    const dist = src.slice(0, src.lastIndexOf('/') + 1).replace('./src/', './dist/')
+    exec(`mkdir -p '${dist}' && npx mjml -w '${src}/*.mjml' -o '${dist}'`)
+    dirs.push(src)
   }
 }
-
-compile()
-
-setInterval(() => {
-  compile()
-}, 1000)
