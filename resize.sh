b7d47213edd3c38a922d1b107c8c69c1887f7004
Neeraj Dalal <admin@nrjdalal.com>
2023-05-09 18:05:05 +0530

    change in script

--- resize.sh
+++ resize.sh
@@ -1,5 +1,5 @@
 for i in $(find ./src -type f); do
   if [ ${i##*.} = "jpg" ] || [ ${i##*.} = "png" ] || [ ${i##*.} = "webp" ] && [[ $(identify -format "%w" $i) -gt 500 ]]; then
-    morgify -resize '500x>' $i
+    echo $i && morgify -resize '500x>' $i
   fi
 done
