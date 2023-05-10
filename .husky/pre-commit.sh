9df43018127cfefadfa1f2c1f481a37f4fbc4b63
Neeraj Dalal <admin@nrjdalal.com>
2023-05-10 15:11:51 +0530

    updates

--- .husky/pre-commit.sh
+++ .husky/pre-commit.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-node compile.js
-
 for i in $(find ./src -type f); do
   if [ ${i##*.} = "jpg" ] || [ ${i##*.} = "png" ] || [ ${i##*.} = "webp" ] && [[ $(identify -format "%w" $i) -gt 500 ]]; then
     echo "Resizing $i" && mogrify -resize '500x>' $i
