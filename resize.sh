2a2956e89c1f280d07d7c64d28cadac2430fdf48
Neeraj Dalal <admin@nrjdalal.com>
2023-05-10 15:00:16 +0530

    updates

--- resize.sh
+++ resize.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
+
 for i in $(find ./src -type f); do
   if [ ${i##*.} = "jpg" ] || [ ${i##*.} = "png" ] || [ ${i##*.} = "webp" ] && [[ $(identify -format "%w" $i) -gt 500 ]]; then
     echo "Resizing $i" && mogrify -resize '500x>' $i
   fi
 done
+
+echo
+touch .commit
+exit
